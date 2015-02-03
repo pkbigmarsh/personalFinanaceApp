b401b86975c0be996485add4d468c921459f2a67
Marshall Sumwalt <MarshallSumwalt@gmail.com>
2015-02-02 21:08:04 -0600

    Added base Node and project structure
    I should make this a boilerplate that I can just fork every time I make a
    new web app, but this is copied from my personal site project. It sets up
    node, jade, and gulp
    
    NodeAndProjectSetup

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var del     = require('del'),
+    gulp    = require('gulp'),
+    notify  = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    // jade
+    jade    = require('gulp-jade'),
+    // js
+    jshint  = require('gulp-jshint'),
+    uglify  = require('gulp-uglify'),
+
+    // delpoying
+    deploy  = require('gulp-gh-pages');
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('move', 'templates')
+});
+
+
+gulp.task('move', function() {
+        // Resources
+        gulp.src('src/resources/**/*')
+            .pipe(gulp.dest("dist/resources/"))
+});
+
+gulp.task('templates', function() {
+    gulp.src('src/jade/*.jade')
+    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+    .pipe(jade({
+        basedir: './src/jade',
+        pretty: true
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('deploy', function () {
+    return gulp.src("./dist/**/**/*")
+        .pipe(deploy({
+            branch: "master"
+        }))
+});
+
+gulp.task('clean', del.bind(null, ['dist/**/**/*', '!.*']));
